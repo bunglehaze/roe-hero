b942affffa5a000ad51a623a202ca76d6f4c6a63
Leigh Godson <leigh.godson.external@eviden.com>
2023-07-13 12:28:00 +0100

    changed structure

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
-	res.end('Welcome to the new Node');
+	res.end('Welcome to the new Node installation');
 });
 
 server.listen(port, hostname, () => {
